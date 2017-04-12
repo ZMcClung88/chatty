9d0825ffeebbd8123962d0c98e1d4335b0b2a6c5
Zac McClung <zac.mcclung370@gmail.com>
2017-04-12 15:04:46 -0600

    finished problems minus black diamonds

--- assets/scripts/controllers/main.js
+++ assets/scripts/controllers/main.js
@@ -4,12 +4,14 @@ angular.module('chattyApp')
   .controller('MainCtrl', function ( $scope, messageService ) {
     messageService.getMessages().then(function ( response ) {
       $scope.messages = response.data.messages;
+      console.log($scope.messages);
     });
 
     $scope.addMessage = function ( message ) {
       if (message) {
         messageService.addMessage(message).then(function ( response ) {
           $scope.messages = response.data.messages;
+          console.log($scope.messages);
         });
       }
     };
--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+//external dependencies
+var express = require('express');
+var bodyParser = require('body-parser');
 
+//app specific variables
+var app = express();
+var port = 3000;
+
+app.use(express.static('assets'))
+app.use(bodyParser.json())
+
+var messages = [];
+
+app.get('/messages', function (req, res, next) {
+  res.status(200).json({ messages: messages });
+});
+
+app.post('/messages', function (req, res, next) {
+  console.log(req.body);
+  messages.push({ message: req.body.message, time: new Date () });
+  res.status(200).json({messages: messages});
+});
+
+app.listen(port);
